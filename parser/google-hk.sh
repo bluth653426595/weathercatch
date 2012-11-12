87cca98e9d8f555e757caf919bbfe112c52f29f4
Xu FaSheng <xufasheng2006@gmail.com>
2012-11-12 23:59:33 +0800

    * add parser, google-hk

---
+++ parser/google-hk.sh
@@ -0,0 +1,106 @@
+# google-hk.sh ---
+#
+# Filename: google-hk.sh
+# Description: a parser fo weather_catcher
+# Author: Xu FaSheng
+# Created: 2012.11.12
+# Keywords: weather_catcher parser google-hk
+
+# This file is a part of weather_catcher
+
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License as
+# published by the Free Software Foundation; either version 3, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; see the file COPYING.  If not, write to
+# the Free Software Foundation, Inc., 51 Franklin Street, Fifth
+# Floor, Boston, MA 02110-1301, USA.
+#
+#
+
+# Code:
+
+
+# depend on parser google
+parent_script=`get_parser_script google`
+source "$parent_script"
+
+parser_name="google-hk"
+default_arg="weather hongkong"
+debug "parser_name: $parser_name"
+
+update_weather_data () {
+    if [ -z "$arg" ]; then arg="$default_arg"; fi
+
+    URL="http://www.google.com.hk/search?q=$arg"
+    URL=`convert_url_space "$URL"`
+
+    # dump web page
+    debug "dumping url: $URL"
+    web_content=`retry_cmd 3 timeout_cmd 15s w3m -dump -no-cookie "$URL"`
+    if [ -n "$web_content" ]; then
+        echo "$web_content">$weather_tmp_file
+    fi
+
+    parse_data
+}
+
+parse_location () {
+    LN=`echo "$weather_block" | head -1 | sed 's/^.*\s\(.*\)天氣$/\1/'`
+}
+
+simplify_weather_block () {
+    weather_block=`get_section_string "$weather_block" "[0-9]+°C" "濕度："`
+    weather_block=`echo "$weather_block" | sed -e 's/^ *• \?//'`
+    weather_block=`echo "$weather_block" | sed 's/^\(\S\+\s\)\+//'`
+
+    # delete unicode characters, 0x200e
+    weather_block=`echo "$weather_block" | sed 's/‎\(.\)/\1/g'`
+
+    # delete prefix spaces
+    weather_block=`echo "$weather_block" | sed 's/^ \+//'`
+
+    debug "weather block[fixed]"
+    debug_lines "$weather_block"
+}
+
+parse_left_weather_block () {
+    # get weather tag
+    local weather_tag=`echo "$left_weather_block" | sed -n '/\[.*\]/p' | sed 's/\(\[.*\]\).*/\1/'`
+    left_weather_block=`echo "$left_weather_block" | sed 's/\[.*\]//'`
+
+    # current weather font
+    CWF=`google_weather_tag2wf "$weather_tag"`
+
+    # current temperature
+    CT=`echo "$left_weather_block" | sed -n 1p | tr -d '°C'`
+
+    # current weather text
+    CWT=`echo "$left_weather_block" | sed -n 2p | sed -e 's/^\s*//' -e 's/\s*$//'`
+
+    # wind direction
+    WD=`echo "$left_weather_block" | sed -n 3p | awk -F'，' '{print $1}' | awk -F'：' '{print $2}'`
+
+    # wind speed
+    WS=`echo "$left_weather_block" | sed -n 3p | awk -F'，' '{print $2}' | awk -F'：' '{print $2}' | awk '{print $1}'`
+
+    # humidity
+    HM=`echo "$left_weather_block" | sed -n '$p' | awk -F'：' '{print $2}' | tr -d '%'`
+}
+
+parser_version () {
+    echo "build 20121112"
+}
+
+debug "parser load success"
+
+
+#
+# google-hk.sh ends here
